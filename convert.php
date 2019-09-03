8ecbf291b56c9b36387f2f4d501a4383db5e73b1
Ioannis Kokkinis <info@upg.gr>
2019-09-03 10:53:25 +0300

    f
    d

---
+++ convert.php
@@ -0,0 +1,40 @@
+<?php
+
+#copy_all_lines(666);
+remove_faulty_lines('ptrac3344027109318363692','ptrac3344027109318363692_out.csv');
+
+
+
+function copy_all_lines($position) {
+$keimena = glob("path/*.txt");
+foreach($keimena as $keimeno_fname) {
+$keimeno =  file_get_contents($keimeno_fname);
+$grammes = explode("\n", $keimeno);
+$grammi = $grammes[$position];
+file_put_contents("teliko_keimeno.txt", $grammi, FILE_APPEND);
+}
+}
+
+function remove_faulty_lines($file_in,$file_out){
+  if ($fh = fopen($file_in, 'r')) {
+    $fg = fopen("$file_out", "w");
+      while (!feof($fh)) {
+          $line = fgets($fh);
+          $numbers = explode(" ", trim($line));
+          $l_array=array();
+          foreach ($numbers as &$number)
+          {
+            if ($number != '') {
+              array_push($l_array,floatval($number));
+            }
+
+          }
+          fwrite($fg, implode(",",$l_array));
+          fwrite($fg, "\n");
+      }
+      fclose($fh);
+      fclose($fg); 
+  }
+
+}
+?>
