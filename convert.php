f86048489f2b44a7da7c86786b0100c438d70afa
Ioannis Kokkinis <info@upg.gr>
2019-09-03 11:42:16 +0300

    Updated convert.php.

--- convert.php
+++ convert.php
@@ -17,6 +17,7 @@ file_put_contents("teliko_keimeno.txt", $grammi, FILE_APPEND);
 
 function remove_faulty_lines($file_in,$file_out,$threshold){
   if ($fh = fopen($file_in, 'r')) {
+    $file_out = $file_in.'processed';
     unlink($file_out);
     $fg = fopen("$file_out", "w");
       while (!feof($fh)) {
